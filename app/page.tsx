676cc02f4ab9573e9090b9fb1102e68ecf106ed3
princethila <33749524+princethila@users.noreply.github.com>
2025-01-02 16:04:48 +0200

    some more changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,40 @@ import React, { useState, useEffect } from 'react';
   const calculateTimeElapsed = () =>{
     if (!selectedDate) return
 
-    const inputDate = new Date(selectedDate)
-    const now = new Date()
+    const inputDate = new Date(selectedDate);
+    const now = new Date();
+    const diffTime = Math.abs(now - inputDate);
+
+    const totalSeconds = Math.floor(diffTime / 1000);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const totalHours = Math.floor(totalMinutes / 60);
+    const totalDays = Math.floor(totalHours / 24);
+
+
+    setTimeElapsed({ 
+      days: totalDays,
+      hours: totalHours,
+      minutes: totalMinutes,
+      seconds: totalSeconds
+    });
+
+    useEffect(() => {
+      let interval;
+      if (selectedDate) {
+        calculateTimeElapsed();
+        interval = setInterval(calculateTimeElapsed, 1000);
+      }
+      return () => clearInterval(interval);
+    }, [selectedDate]);
+
+    const formatDate = (dateString) => {
+      const date = new Date(dateString);
+      return date.toLocaleDateString('en-GB', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      });
+    };
   }
   return (
     <div className="">
