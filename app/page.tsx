b98a937ae08fd4c9e3d46776d86ef89a070dd751
princethila <33749524+princethila@users.noreply.github.com>
2025-01-02 13:46:40 +0200

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+"use client"
+import React, { useState, useEffect } from 'react';
+ 
+ export default function Home() {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [timeElapsed, setTimeElapsed] = useState(null);
+
+  const calculateTimeElapsed = () =>{
+    if (!selectedDate) return
+
+    const inputDate = new Date(selectedDate)
+    const now = new Date()
+  }
   return (
     <div className="">
       we up!
